ce5dc46dfcdc42c6afd9b316d8e03371f686cacc
Nicolas Mai <nicolas.mail@hec.edu>
2016-01-21 17:31:52 -0800

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,15 +21,15 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    // Iterate of todos array, Find the match
-     
-    todos.forEach(function (todo) {
-     
-       if (todoId === todo.id) {
-           matchedTodo = todo;
-       } 
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+ 
+    //  
+    // todos.forEach(function (todo) {
+    //  
+    //    if (todoId === todo.id) {
+    //        matchedTodo = todo;
+    //    } 
+    // });
     
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -40,10 +41,15 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos/:id
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
     
-    // add id field
-    // push body into array
+    if (!_.isBoolean(body.completed || !_.isString(body.description)) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    
+    // set body.description to be trimmed value
+    
+    body.description = body.description.trim();
     
     body.id = todoNextId;
     todoNextId ++;
